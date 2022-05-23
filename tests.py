8717976c57873d3fc4d2c5d2a25830a4ed1d15bf
jon_edward <arithmatlic@gmail.com>
2022-05-23 17:40:52 -0400

    tests formatting

--- tests.py
+++ tests.py
@@ -1,7 +1,11 @@
+"""
+Tests for current_path.
+"""
+
 import os
 from pathlib import Path
 from unittest import TestCase
-from current_path import *
+from current_path import current_file, current_dir, current_dir_as_cwd
 
 
 CURRENT_PATH_TRUTH = Path(__file__).absolute()
@@ -9,6 +13,8 @@ TEST_DIR = Path("test_dir/").absolute()
 
 
 class TestCurrentPath(TestCase):
+    """TestClass for testing methods in current_path"""
+
     def setUp(self) -> None:
         os.mkdir(TEST_DIR)
 
@@ -16,16 +22,25 @@ class TestCurrentPath(TestCase):
         os.rmdir(TEST_DIR)
 
     def test_current_file(self):
+        """
+        Test current_file method of current_path.
+        """
         self.assertEqual(current_file(), CURRENT_PATH_TRUTH, "current_file must return the same "
                                                              "path as __file__")
 
     def test_current_dir(self):
+        """
+        Test current_dir method of current_path.
+        """
         self.assertEqual(current_dir(),
                          Path(CURRENT_PATH_TRUTH).parent.absolute(), "current_dir must return the "
                                                                      "same path as parent "
                                                                      "of __file__")
 
     def test_current_dir_as_cwd(self):
+        """
+        Test current_dir_as_cwd method of current_path.
+        """
         os.chdir(TEST_DIR)
         with current_dir_as_cwd():
             self.assertEqual(Path(os.getcwd()).absolute(), CURRENT_PATH_TRUTH.parent.absolute(),
@@ -33,6 +48,10 @@ class TestCurrentPath(TestCase):
                              "current working directory within the code block under the CM")
 
     def test_current_dir_as_cwd_resetting_cwd(self):
+        """
+        Test current_dir_as_cwd method of current_path, resetting cwd back to cwd before context
+        manager.
+        """
         os.chdir(TEST_DIR)
         with current_dir_as_cwd():
             pass
@@ -40,5 +59,3 @@ class TestCurrentPath(TestCase):
                                                                  "context manager should reset "
                                                                  "the cwd after code block under "
                                                                  "CM has been exited")
-
-
