7dc65db7d5fb70935fe67be83d76c6ef1872a5e2
jon_edward <arithmatlic@gmail.com>
2022-05-25 13:36:54 -0400

    new version - readme, classifiers

--- setup.py
+++ setup.py
@@ -7,13 +7,29 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
+classifiers = """\
+Operating System :: OS Independent
+Intended Audience :: Developers
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
+Programming Language :: Python :: 3.11
+Programming Language :: Python :: Implementation :: CPython
+Programming Language :: Python :: Implementation :: PyPy
+License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+"""
+
 setuptools.setup(
     name="current-path",
-    version="0.0.3",
+    version="0.0.4",
     author="jon-edward",
     author_email="arithmatlic@gmail.com",
     description="A small library for getting current path data "
                 "for a script that imports this library.",
+    classifiers=classifiers.splitlines(),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jon-edward/current_path",
